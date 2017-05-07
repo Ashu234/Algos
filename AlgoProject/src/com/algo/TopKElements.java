aa620ea59975a4cfe7f32ebf7db446ba2d34ccdb
ashu <ashu@Kharayat>
2017-05-07 02:24:50 -0500

    removed the error due to duplicate elements.

--- AlgoProject/src/com/algo/TopKElements.java
+++ AlgoProject/src/com/algo/TopKElements.java
@@ -8,14 +8,27 @@ public class TopKElements {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] arr1 = {7, 11 , 3, 15, 20 , 2, 10, 9, 6, 18, 13};
+		int[] arr1 = {6, 11 , 3, 15, 20 , 20, 10, 9, 6, 18, 13};
 		int size = arr1.length;
 		
 		buildHeap(arr1,size);
-		int topK = 3;
+		
+		printHeap(arr1);
+		int topK = 11;
 		
 		findTopKElements(arr1,size,topK);
+		
+		printHeap(arr1);
 
+	}
+
+	private static void printHeap(int[] arr1) {
+		// TODO Auto-generated method stub
+		for(int i = 0;i < arr1.length; i ++)
+		{
+			System.out.println(arr1[i]);
+		}
+		
 	}
 
 	private static void findTopKElements(int[] arr1, int N, int k) {
@@ -29,9 +42,12 @@ public class TopKElements {
 			arr1[0] = arr1[size];
 			arr1[size] = temp;
 			
-			System.out.println(i-k + "st top element :" + arr1[size]);
 			size = size -1;
+			if(size > 0)
+			{
 			heapify(arr1, 0 , size);
+			}
+			System.out.println(i-k + "st top element :" + arr1[size+1]);
 			k--;
 			
 		}
@@ -56,13 +72,13 @@ public class TopKElements {
 		int right = 2*i + 2;
 		int swapIndex = i;
 		
-		if(left < N && arr1[left] > largest)
+		if((left < N || left == 1 )&& arr1[left] > largest)
 		{
 			largest = arr1[left];
 			swapIndex = left;
 		}
 		
-		if(right < N && arr1[right] > largest)
+		if(right <= N && arr1[right] > largest)
 		{
 			largest = arr1[right];
 			swapIndex = right;
